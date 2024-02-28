b1d79ab97f1f1775b4ac5bd575704c568e8b0228
Arthur <74242616+MrFreedy@users.noreply.github.com>
2024-02-28 23:29:14 +0100

    Missing build.gradle.kts in previous commit

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
     id("java")
+    id("checkstyle")
 }
 
 group = "org.azal"
 version = "1.0-SNAPSHOT"
+
+apply(plugin = "checkstyle")
 
 repositories {
     mavenCentral()
@@ -16,4 +19,16 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+checkstyle {
+    toolVersion = "8.45"
+    configFile = file("config/checkstyle/checkstyle.xml")
+    val mainSourceSet = project.sourceSets.getByName("main")
+    sourceSets = listOf(mainSourceSet)
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    configFile = file("config/checkstyle/checkstyle.xml")
+    ignoreFailures = false
 }
